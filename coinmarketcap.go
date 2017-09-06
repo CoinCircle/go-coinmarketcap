97510c1b535b21453fe8c16790fed05e684774fb
Miguel Mota <miguelmota2@gmail.com>
2017-09-05 21:26:36 -0700

    typo

--- coinmarketcap.go
+++ coinmarketcap.go
@@ -10,7 +10,7 @@ import (
 
 var (
 	baseUrl = "https://api.coinmarketcap.com/v1"
-	graphsUrl = "https://graphs.coinmarketcap.com/currencies"
+	graphUrl = "https://graphs.coinmarketcap.com/currencies"
 	url     string
 )
 
